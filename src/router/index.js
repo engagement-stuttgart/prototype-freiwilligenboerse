49a08d2b02d1eddff4c39294c4dda8a0a9c7f654
rashfael <rashfael@isobeef.org>
2020-01-15 20:52:22 +0100

    add category list on landing page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from 'views/Home'
+import Category from 'views/category'
 import Job from 'views/job'
 
 Vue.use(VueRouter)
@@ -10,6 +11,11 @@ const routes = [
 		path: '/',
 		name: 'home',
 		component: Home
+	}, {
+		path: '/category/:categoryId',
+		name: 'category',
+		component: Category,
+		props: true
 	}, {
 		path: '/jobs/:jobId',
 		name: 'job',
--- src/views/category.vue
+++ src/views/category.vue
@@ -4,20 +4,19 @@
 		.hero
 			h1 Some flavor text
 			p A lot more flavor text. A lot more flavor text. A lot more flavor text. A lot more flavor text. A lot more flavor text. A lot more flavor text.
-		.categories
-			router-link.category(v-for="category of categories", :to="{name: 'category', params: {categoryId: category.id}}")
-				img(:src="`https://placeimg.com/200/160/any?${category.id}`")
-				.name {{ category.label }}
+		bunt-input(name="search", label="Suche", v-model="search")
+		bunt-progress-circular(v-if="jobsLoading", size="huge")
+		.list(v-else)
+			router-link.job(v-for="job of jobs", :to="{name: 'job', params: {jobId: job.id}}", :style="{'background-image': `url(https://placeimg.com/640/480/any?${job.id})`}")
+				.name {{ job.titel }}
 </template>
 <script>
 import api from 'lib/api'
-import categories from 'assets/categories.json'
 
 export default {
 	name: 'home',
 	data () {
 		return {
-			categories: Object.entries(categories).map(cat => ({id: cat[0], label: cat[1]})),
 			search: '',
 			jobs: [],
 			jobsLoading: false,
@@ -63,7 +62,6 @@ export default {
 		align-self: center
 		width: 100%
 		box-sizing: border-box
-		background-color: $clr-white
 		+above('m')
 			padding: 8px 32px
 		+below('m')
@@ -73,33 +71,27 @@ export default {
 				margin: 0
 		.bunt-progress-circular
 			align-self: center
-		.categories
-			display: grid
-			grid-template-columns: repeat(2, 1fr)
-			// max-width: 100vw
-			grid-column-gap: 8px
-			grid-row-gap: 8px
-			.category
-				height: 128px + 56px
+		.list
+			display: flex
+			flex-direction: column
+			.job
+				height: 200px
+				// line-height: 32px
+				margin: 8px 0
 				position: relative
 				display: flex
 				flex-direction: column
 				justify-content: flex-end
 				img
-					height: 128px
+					position: absolute
+					height: 180px
 					object-fit: cover
-					border-radius: 8px
 				.name
-					font-size: 14px
-					padding: 4px 8px 8px
-					height: 56px
-					vertical-align: middle
-					background-color: $clr-primary-text-dark
+					font-size: 18px
+					padding: 16px 8px
+					background-color: $clr-secondary-text-dark
 					// font-weight: 500
 					color: $clr-primary-text-light
-					overflow-wrap: break-word
-					// border: 2px solid $clr-grey-500
-					// border-top: none
 				// & + .job
 				// 	border-top: border-separator()
 </style>
